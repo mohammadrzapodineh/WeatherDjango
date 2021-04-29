b28c942ee87ebcca6c22ffd086dc628ac4bd29ce
mohammad <mohammadrza@gmail.com>
2021-04-29 14:15:31 -0700

    Updated Urls

--- WeatherDjango/urls.py
+++ WeatherDjango/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('UMHMDPNl453/', admin.site.urls),
     path('', include('Weather.urls'))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
